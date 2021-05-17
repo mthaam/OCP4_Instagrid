6fae3271ccc16ed1ff59d7cad3120d9e9ae03736
mthaam <jsbrunet@protonmail.com>
2021-05-17 21:01:59 +0200

    added code to make an action after tap gesture in grid. added a picture for layout buttons.

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -8,10 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    private let imagePickerController = UIImagePickerController()
     
-    @IBOutlet weak var blueView: PhotosGridCustomView!
+    // ==============================================
+    // MARK: - Private Properties
+    // ==============================================
+    
+    private let imagePickerController = UIImagePickerController()
+    private var style: PhotoGridStackViewStyle = .fourSquares {
+        didSet {
+            setStyle(style: style)
+        }
+    }
+    private var selectedPlusImage: UIImageView?
+    
+    // ==============================================
+    // MARK: - Outlets
+    // ==============================================
+    
+    @IBOutlet weak var blueView: UIView!
+    @IBOutlet var topStackView: UIStackView!
+    @IBOutlet var bottomStackView: UIStackView!
     @IBOutlet weak var topLeft_UIImage: UIImageView!
     @IBOutlet weak var topRight_UIImage: UIImageView!
     @IBOutlet weak var bottomLeft_UIImage: UIImageView!
@@ -19,59 +35,112 @@ class ViewController: UIViewController {
     @IBOutlet var layoutButtonsArray: [UIButton]!
     @IBOutlet weak var swipeToShare_StackView: UIStackView!
     
+    // ==============================================
+    // MARK: - Function viewDidLoad()
+    // ==============================================
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapAddsPhotoFromAlbum(_:)))
-        topLeft_UIImage.addGestureRecognizer(UITapGestureRecognizer)
-        topRight_UIImage.addGestureRecognizer(UITapGestureRecognizer)
-        bottomLeft_UIImage.addGestureRecognizer(UITapGestureRecognizer)
-        bottomRight_UIImage.addGestureRecognizer(UITapGestureRecognizer)
+        
+        photogridTypeButtonTouched(layoutButtonsArray[1])
+        
+        let TL_UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapAddsPhotoFromAlbum(_:)))
+        topLeft_UIImage.addGestureRecognizer(TL_UITapGestureRecognizer)
+        let TR_UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapAddsPhotoFromAlbum(_:)))
+        topRight_UIImage.addGestureRecognizer(TR_UITapGestureRecognizer)
+        let BT_UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapAddsPhotoFromAlbum(_:)))
+        bottomLeft_UIImage.addGestureRecognizer(BT_UITapGestureRecognizer)
+        let BR_UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTapAddsPhotoFromAlbum(_:)))
+        bottomRight_UIImage.addGestureRecognizer(BR_UITapGestureRecognizer)
         
         let swipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeGestureToShareView(_:)))
         swipeToShare_StackView.addGestureRecognizer(swipeGestureRecognizer)
+        
+        imagePickerController.delegate = self
+        
     }
-    
-    @IBAction func photogridTypeButtonTouched(_ sender: UIButton) {
-        layoutButtonsArray.forEach { button in
-            button.isSelected = false
+
+    // ==============================================
+    // MARK: - Enum and func to set grid type
+    // ==============================================
+
+    enum PhotoGridStackViewStyle {
+        case fourSquares, twoSquaresUp, twoSquaresBottom
+    }
+
+    private func setStyle(style: PhotoGridStackViewStyle) {
+        switch style {
+        case .fourSquares:
+            topStackView.arrangedSubviews[0].isHidden = false
+            bottomStackView.arrangedSubviews[0].isHidden = false
+        case .twoSquaresBottom :
+            topStackView.arrangedSubviews[0].isHidden = true
+            bottomStackView.arrangedSubviews[0].isHidden = false
+        case .twoSquaresUp :
+            bottomStackView.arrangedSubviews[0].isHidden = true
+            topStackView.arrangedSubviews[0].isHidden = false
         }
+    }
+
+    // ==============================================
+    // MARK: - @IBActions
+    // ==============================================
+
+    @IBAction func photogridTypeButtonTouched(_ sender: UIButton) {
+        layoutButtonsArray.forEach { button in button.isSelected = false }
         switch sender.tag {
         case 0:
-            blueView.style = .twoSquaresBottom
+            style = .twoSquaresBottom
         case 1:
-            blueView.style = .twoSquaresUp
+            style = .twoSquaresUp
         case 2 :
-            blueView.style = .fourSquares
+            style = .fourSquares
         default:
-            blueView.style = .fourSquares
+            style = .fourSquares
         }
         setSelectedAppereanceForLayoutButton(sender: sender.tag)
     }
-    
+
+    // ==============================================
+    // MARK: - Private Functions
+    // ==============================================
+
     private func setSelectedAppereanceForLayoutButton(sender: Int) {
         layoutButtonsArray[sender].isSelected = true
-        layoutButtonsArray[sender].imageView?.contentMode = .scaleAspectFill
-        layoutButtonsArray[sender].setImage(#imageLiteral(resourceName: "Selected"), for: .selected)
+        layoutButtonsArray[sender].imageView?.contentMode = .scaleAspectFit
+        layoutButtonsArray[sender].setImage(#imageLiteral(resourceName: "Selected_1024"), for: .selected)
     }
-    
-    @objc func handleTapAddsPhotoFromAlbum(_ sender: UITapGestureRecognizer) {
-        if sender.state == .ended {
-            openUserPhotoAlbum()
-            }
-        
-    }
+
     private func openUserPhotoAlbum() {
-//        imagePickerController.sourceType = .photoLibrary
-//        imagePickerController.allowsEditing = true
-//        imagePickerController.delegate = self
-//        present(imagePickerController, animated: true)
+//
     }
-    
-    
+
+    // ==============================================
+    // MARK: - @objc Functions
+    // ==============================================
+    @objc func handleTapAddsPhotoFromAlbum(_ sender: UITapGestureRecognizer) {
+        print("tapGestureIsWorking")
+        selectedPlusImage = topLeft_UIImage
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.allowsEditing = true
+        present(imagePickerController, animated: true)
+        
+//            openUserPhotoAlbum()
+    }
+
     @objc func handleSwipeGestureToShareView(_ sender: UISwipeGestureRecognizer ) {
 //        let activityViewController = UIActivityViewController(activityItems: [blueView!], applicationActivities: nil)
 //        present(<#T##viewControllerToPresent: UIViewController##UIViewController#>, animated: <#T##Bool#>, completion: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
     }
     
 }
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.editedImage] as? UIImage else { return }
+        selectedPlusImage?.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+}
+
